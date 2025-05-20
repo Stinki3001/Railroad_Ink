a1e4bc7ca44d1e1f0ff71e4b7fd6ffa810124247
Fietje <costraufietje@gmail.com>
2025-05-20 21:02:23 +0200

    Exeption geändert

--- src/main/java/org/costr/railroad_ink/Feld.java
+++ src/main/java/org/costr/railroad_ink/Feld.java
@@ -1,6 +1,7 @@
 package org.costr.railroad_ink;
 
 import java.awt.Image;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
@@ -25,7 +26,7 @@ public class Feld {
         // Default constructor
     }
 
-    Feld Feld1(int index) {
+    Feld Feld1(int index) throws IOException {
         return (switch (index) {
             case 0 -> ZugKurve();
             case 1 -> ZugKreuzung();
@@ -40,86 +41,77 @@ public class Feld {
         });
     }
 
-    private Feld ZugKurve() {
+    private Feld ZugKurve() throws IOException {
         Feld feld = new Feld();
         feld.id = 0;
         feld.name = "ZugKurve";
-        try {
-            feld.image = ImageIO.read(getClass().getResourceAsStream("number-1.JPG"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-1.JPG"));
+
         return feld;
     }
 
-    private Feld ZugKreuzung() {
+    private Feld ZugKreuzung() throws IOException {
         Feld feld = new Feld();
         feld.id = 1;
         feld.name = "ZugKreuzung";
-        try {
-            feld.image = ImageIO.read(getClass().getResourceAsStream("number-2.JPG"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-2.JPG"));
         return feld;
     }
 
-    private Feld ZugGerade() {
+    private Feld ZugGerade() throws IOException {
         Feld feld = new Feld();
         feld.id = 2;
         feld.name = "ZugGerade";
-        try {
-            feld.image = ImageIO.read(getClass().getResourceAsStream("number-3.JPG"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-3.JPG"));
         return feld;
     }
 
-    private Feld StraßeKurve() {
+    private Feld StraßeKurve() throws IOException {
         Feld feld = new Feld();
         feld.id = 3;
         feld.name = "StraßeKurve";
-        try {
-            feld.image = ImageIO.read(getClass().getResourceAsStream("number-4.JPG"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-4.JPG"));
         return feld;
     }
 
-    private Feld StraßeKreuzung() {
+    private Feld StraßeKreuzung() throws IOException {
         Feld feld = new Feld();
         feld.id = 4;
         feld.name = "StraßeKreuzung";
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-5.JPG"));
         return feld;
     }
 
-    private Feld StraßeGerade() {
+    private Feld StraßeGerade() throws IOException {
         Feld feld = new Feld();
         feld.id = 5;
         feld.name = "StraßeGerade";
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-6.JPG"));
         return feld;
     }
 
-    private Feld StraßeMitBrücke() {
+    private Feld StraßeMitBrücke() throws IOException {
         Feld feld = new Feld();
         feld.id = 6;
         feld.name = "Straße mit ZugBrücke";
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-7.JPG"));
         return feld;
     }
 
-    private Feld BahnhofGerade() {
+    private Feld BahnhofGerade() throws IOException {
         Feld feld = new Feld();
         feld.id = 7;
         feld.name = "BahnhofGerade";
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-8.JPG"));
         return feld;
     }
 
-    private Feld BahnhofKurve() {
+    private Feld BahnhofKurve() throws IOException {
         Feld feld = new Feld();
         feld.id = 8;
         feld.name = "BahnhofKurve";
+        feld.image = ImageIO.read(getClass().getResourceAsStream("number-9.JPG"));
         return feld;
     }
 
